d13dcf704bad8d90a11d74728bda9a7c0ec50389
krystenharvey <krysten.harvey@vanderbilt.edu>
2016-06-23 17:45:52 -0500

    6/23/2016

---
+++ www/js/TP53.php
@@ -0,0 +1,254 @@
+{
+  "records": [
+  {
+    "Mutation": "c.?",
+    "ID": 43617,
+    "Count": 456
+  },
+  {
+    "Mutation": "c.818G>T",
+    "ID": 10779,
+    "Count": 83
+  },
+  {
+    "Mutation": "c.818G>A",
+    "ID": 10660,
+    "Count": 77
+  },
+  {
+    "Mutation": "c.743G>T",
+    "ID": 6549,
+    "Count": 53
+  },
+  {
+    "Mutation": "c.469G>T",
+    "ID": 10670,
+    "Count": 50
+  },
+  {
+    "Mutation": "c.733G>T",
+    "ID": 11081,
+    "Count": 44
+  },
+  {
+    "Mutation": "c.473G>T",
+    "ID": 10714,
+    "Count": 42
+  },
+  {
+    "Mutation": "c.743G>A",
+    "ID": 10662,
+    "Count": 32
+  },
+  {
+    "Mutation": "c.747G>T",
+    "ID": 10817,
+    "Count": 30
+  },
+  {
+    "Mutation": "c.659A>G",
+    "ID": 10758,
+    "Count": 27
+  },
+  {
+    "Mutation": "c.488A>G",
+    "ID": 10808,
+    "Count": 26
+  },
+  {
+    "Mutation": "c.742C>T",
+    "ID": 10656,
+    "Count": 21
+  },
+  {
+    "Mutation": "c.844C>T",
+    "ID": 10704,
+    "Count": 21
+  },
+  {
+    "Mutation": "c.734G>T",
+    "ID": 11196,
+    "Count": 21
+  },
+  {
+    "Mutation": "c.461G>T",
+    "ID": 6815,
+    "Count": 19
+  },
+  {
+    "Mutation": "c.524G>A",
+    "ID": 10648,
+    "Count": 18
+  },
+  {
+    "Mutation": "c.701A>G",
+    "ID": 10725,
+    "Count": 18
+  },
+  {
+    "Mutation": "c.817C>T",
+    "ID": 10659,
+    "Count": 17
+  },
+  {
+    "Mutation": "c.725G>T",
+    "ID": 10810,
+    "Count": 17
+  },
+  {
+    "Mutation": "c.527G>T",
+    "ID": 10645,
+    "Count": 16
+  },
+  {
+    "Mutation": "c.641A>G",
+    "ID": 43687,
+    "Count": 15
+  },
+  {
+    "Mutation": "c.746G>T",
+    "ID": 43871,
+    "Count": 15
+  },
+  {
+    "Mutation": "c.892G>T",
+    "ID": 10710,
+    "Count": 15
+  },
+  {
+    "Mutation": "c.536A>G",
+    "ID": 10889,
+    "Count": 13
+  },
+  {
+    "Mutation": "c.818G>C",
+    "ID": 43896,
+    "Count": 13
+  },
+  {
+    "Mutation": "c.578A>G",
+    "ID": 10742,
+    "Count": 13
+  },
+  {
+    "Mutation": "c.637C>T",
+    "ID": 10654,
+    "Count": 13
+  },
+  {
+    "Mutation": "c.797G>T",
+    "ID": 10958,
+    "Count": 12
+  },
+  {
+    "Mutation": "c.614A>G",
+    "ID": 43947,
+    "Count": 12
+  },
+  {
+    "Mutation": "c.745A>T",
+    "ID": 43629,
+    "Count": 12
+  },
+  {
+    "Mutation": "c.?",
+    "ID": 96438,
+    "Count": 11
+  },
+  {
+    "Mutation": "c.574C>T",
+    "ID": 10733,
+    "Count": 11
+  },
+  {
+    "Mutation": "c.527G>A",
+    "ID": 10687,
+    "Count": 11
+  },
+  {
+    "Mutation": "c.711G>A",
+    "ID": 10834,
+    "Count": 11
+  },
+  {
+    "Mutation": "c.733G>A",
+    "ID": 6932,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.517G>T",
+    "ID": 43559,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.375G>T",
+    "ID": 45940,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.848G>C",
+    "ID": 10743,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.610G>T",
+    "ID": 10804,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.430C>T",
+    "ID": 11245,
+    "Count": 10
+  },
+  {
+    "Mutation": "c.745A>G",
+    "ID": 10668,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.991C>T",
+    "ID": 11354,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.586C>T",
+    "ID": 10705,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.535C>T",
+    "ID": 10768,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.747G>C",
+    "ID": 10785,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.476C>T",
+    "ID": 11148,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.451C>T",
+    "ID": 10905,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.463A>C",
+    "ID": 10912,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.1010G>T",
+    "ID": 11411,
+    "Count": 9
+  },
+  {
+    "Mutation": "c.839G>T",
+    "ID": 11287,
+    "Count": 8
+  }
+]
+}
--- www/js/app.js
+++ www/js/app.js
@@ -1,50 +1,114 @@
 var app = angular.module('myApp', []);
 app.controller('MyCtrl', function($scope, $http) {
-  $http.get("TP53.json").then(function (response) {
-      $scope.myVariants = response.data.TP53;
-      var trace1 = {
-         x: [],
-         y: [],
-         text: [],
-         // initial text -->text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-         mode: [],
-         marker: {
-           color:'',
-           size: 0
-         }
-       };
-       for (var i in $scope.variants)
-       {
-         trace1.x[i]= i;
-         trace1.y[i]=i;
-         trace1.text[i]=i.'CDS Mutation'+" "+i.'Mutation ID (COSM)';
-         trace1.mode[i]= 'markers';
-         trace1.marker[i]={color:['rgb(93, 164, 214)'],size:i.'Count'});
+  console.log("hello in function");
+
+
+  var trace1 = {
+      type: 'scatter',
+      line:false,
+       x: [],
+       y: [],
+       text: [],
+       // initial text -->text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+       mode: 'markers',
+       marker:{
+         sizemode: "area",
+         sizeref: 0.0006,
+         color:[],
+         size: []
        }
-  });
+     };
+  $http.get("js/TP53.php").then(function (response) {
+      console.log("hello in function");
+      $scope.myVariants = response.data.records;
+      console.log("hello in function");
+    //  console.log($scope.myVariants[0].ID);
+    //  console.log($scope.myVariants.length);
+
+      var array = [];
+      for (var i =0; i<$scope.myVariants.length;i++)
+      {
+        array[i] = $scope.myVariants[i].Count;
+      }
+
+      var largest =1;
+
+      for (i=0; i<$scope.myVariants.length;i++){
+        if (array[i]>largest) {
+        largest=array[i];
+    }
+}
+//  console.log(largest);
+
+var count = 0;
+var counter =0;
+for (i =0; i<$scope.myVariants.length;i++)
+{
+
+        trace1.x[count]= (counter);
+      //  console.log(trace1.x[count]);
+        trace1.y[count]= $scope.myVariants[i].Count
+        //console.log(trace1.y[count]);
+        trace1.text[count]="Mutation: "+ $scope.myVariants[i].Mutation+"\n"+
+        "ID: "+$scope.myVariants[i].ID;
+        //console.log(trace1.text[count]);
+
+        //trace1.mode[count]= 'markers';
+        //console.log(trace1.mode[i]);
+        if (count%2 ==0)
+        trace1.marker.color[count]='rgb(93, 184, 214)';
+        if (count%2 !=0)
+        trace1.marker.color[count]='rgb(255, 144, 14)';
+
+        if (count!=0)
+        trace1.marker.size[count]=(($scope.myVariants[i].Count)*10)/$scope.myVariants[0].Count;
+        else {
+          trace1.marker.size[count]=5;
+        }
+
+       //console.log(trace1.marker.color[count]);
+      //console.log(trace1.marker.size[count]);
+      count++;
+      counter +=10;
+
+}
+
+//console.log(trace1.y.length);
+//$scope.iframeHeight = $(document).height();
+
+var data = [trace1];
+
+var layout = {
+  title: 'TP53 Variants',
+ autosize: true,
+ width: 1050,
+ height:800,
+  showlegend: false,
+  xaxis: {
+    type: 'log'
+  },
+  yaxis: {
+    type: 'log'
+  }
+};
+
+
+
+$(document).ready(function(){
+
+Plotly.plot(document.getElementById("tester"), data, layout);
+var myPlot = document.getElementById('tester');
+myPlot.on('plotly_click', function(data){
+    var pts = '';
+    for(var i=0; i < data.points.length; i++){
+        pts = 'x = '+data.points[i].x +'\ny = '+
+            data.points[i].y.toPrecision(4) + '\n\n';
+    }
+    alert('Closest point clicked:\n\n'+pts);
 });
 
 
- /*var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-    // initial text -->text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      size: [40, 60, 80, 100]
-    }
-  };
 
-  var data = [trace1];
-
-  var layout = {
-    title: 'TP53 Variants',
-    showlegend: false,
-    height: 600,
-    width:600 };
-*/
-$(document).ready(function(){
-
-Plotly.plot(document.getElementById("tester"), data, layout); })});
+ })
+});
+   });
